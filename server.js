ddd0f5cd52bc536e08ba24ff0ef2f49b836424c4
Greg <gagnew99@gmail.com>
2021-03-17 19:16:40 -0400

    new twitter search query

--- server.js
+++ server.js
@@ -5,28 +5,23 @@ const client = new Twitter({
   bearer_token: 'AAAAAAAAAAAAAAAAAAAAAL28NgEAAAAACcfmoDaF5uoF0gkHBUhnXLibdUM%3D7haLn1q0CKqx9N0VswPkpidtfKEBxf5jHKNoOzqG7df7kcOld3',
 })
 
-// client.get('tweets/search/recent', {
-//   query: '#eisc'
-// }).then((resolved) => {
-//   console.log('resolved')
-//   console.log(resolved)
-// }, (rejected) => {
-//   console.log('rejected')
-//   console.log(rejected)
+client.get('tweets/search/recent?tweet.fields=id,text,attachments,author_id,created_at,entities,geo,lang,public_metrics,referenced_tweets', {
+  query: '#eisc'
+}).then((resolved) => {
+  console.log('resolved')
+  console.log(resolved)
+}, (rejected) => {
+  console.log('rejected')
+  console.log(rejected)
+})
+
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'ianneedswork',
+//   database: 'twitter'
 // })
-
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ianneedswork',
-  database: 'twitter'
-})
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-  if (error) console.log(error)
-  else console.log('The result is: ', results[0])
-})
-
-connection.end()
+//
+// connection.connect();
+//
+// connection.end()
